b939a5f7ef8de9fd73a57bb73f93a3a9c55efb62
Jose Zaltar <josezaltar@gmail.com>
2025-10-01 09:42:10 -0300

    Corrigir domínio Netlify

--- api/twiclone/settings.py
+++ api/twiclone/settings.py
@@ -102,16 +102,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://twiclone.netlify.app",
+    "https://twiiclone.netlify.app",  # ← Adicionar o segundo "i"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://twiclone.netlify.app",
+    "https://twiiclone.netlify.app",  # ← Adicionar o segundo "i"
     "https://josezaltar.pythonanywhere.com",
 ]
-
 CORS_ALLOW_CREDENTIALS = True
 
 # DRF + JWT
